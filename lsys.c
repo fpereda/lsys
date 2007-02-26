c1135d9273ef60f2ef3761000d0b09f755bac649
Fernando J. Pereda <ferdy@gentoo.org>
2007-02-26 23:25:39 +0100

    Being able to specify a function to process bytes
    Signed-off-by: Fernando J. Pereda <ferdy@gentoo.org>

--- lsys.c
+++ lsys.c
@@ -3,20 +3,22 @@
 
 char *rules['Z' - 'A'];
 
-void compute_figure(const char *current, unsigned depth)
+void compute_figure(const char *current, unsigned depth, int (*process)(int))
 {
+	const char *p;
+
 	if (depth <= 0) {
-		printf("%s", current);
+		for (p = current; *p ; p++)
+			process(*p);
 		return;
 	}
 
-	const char *p;
 	for (p = current; *p ; p++) {
 		char c = toupper(*p);
 		if (isalpha(c) && rules[c]) {
-			compute_figure(rules[c], depth - 1);
+			compute_figure(rules[c], depth - 1, process);
 		} else {
-			putchar(c);
+			process(c);
 		}
 	}
 }
@@ -24,11 +26,11 @@ void compute_figure(const char *current, unsigned depth)
 int main(int argc, char *argv[])
 {
 	char *axiom = "[F]4-F";
-	unsigned depth = 1;
+	unsigned depth = 10;
 
 	rules['F'] = "|[+F][-F]";
 
-	compute_figure(axiom, depth);
+	compute_figure(axiom, depth, putchar);
 
 	putchar('\n');
 
