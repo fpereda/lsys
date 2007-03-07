81ce8ec6df12b0d465b82edf8f1fa6d6190fba14
Fernando J. Pereda <ferdy@gentoo.org>
2007-03-07 12:28:18 +0100

    Really remove the unneeded GTK_WINDOW cast, re add the needed one.
    Signed-off-by: Fernando J. Pereda <ferdy@gentoo.org>

--- src/main.c
+++ src/main.c
@@ -141,10 +141,10 @@ int main(int argc, char *argv[])
 	
 	double max = MAX(max_x - min_x + (MARGIN * 2),
 			max_y - min_y + (MARGIN * 2));
-	gtk_widget_set_size_request(GTK_WINDOW(window),
+	gtk_widget_set_size_request(window,
 			(max_x - min_x + (MARGIN * 2)) / max * opts->xmax,
 			(max_y - min_y + (MARGIN * 2)) / max * opts->ymax);
-	gtk_window_set_resizable(window, FALSE);
+	gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
 	g_signal_connect(window, "destroy",
 			G_CALLBACK(gtk_main_quit), NULL);
