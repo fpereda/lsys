ebf28405026da7234235316b4bafe825daa7223c
Fernando J. Pereda <ferdy@zeus.ferdyx.org>
2007-07-16 00:58:41 +0200

    Simple test for xmalloc
    Signed-off-by: Fernando J. Pereda <ferdy@gentoo.org>

---
+++ lsysutil/test/xfuncs-malloc.c
@@ -0,0 +1,69 @@
+/* vim: set sts=4 sw=4 noet : */
+
+/*
+ * Copyright (c) 2007, Fernando J. Pereda <ferdy@gentoo.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the library nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <ctme/ctme.h>
+
+#include <lsysutil/xfuncs.h>
+
+char *name(void)
+{
+    static char n[] = "malloc wrapper";
+    return n;
+}
+
+int repeat(void)
+{
+    return 1;
+}
+
+void run_test(void)
+{
+	void *p;
+	
+	p = xmalloc(0);
+	CTME_CHECK_EQUAL(p, NULL);
+
+	p = xmalloc(100);
+	CTME_CHECK(p != NULL);
+
+	static const char s[] = "1234567890";
+	const char *src = s;
+	char *dst = p;
+	while (*src)
+		*dst++ = *src++;
+	*dst = 0;
+
+	CTME_CHECK_EQUAL(strcmp(s, p), 0);
+	free(p);
+}
